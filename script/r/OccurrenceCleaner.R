b5dd862325797acc2a644af0eeb2e878a052ea0f
Winand <winand@live.nl>
2019-07-31 14:06:24 +0200

    Changed the r script into a function
    Occurrence filter now is a function that takes a filepath (to a .csv file) as input, removes unwanted occurrences and writes the output to a new csv file.

--- script/r/OccurrenceCleaner.R
+++ script/r/OccurrenceCleaner.R
@@ -1,21 +1,28 @@
 library("CoordinateCleaner")
 
-dat = read.csv("./data/maize/zea_mays_l/zea_mays_l_test.csv", header = TRUE)
+OccurenceCleaner <- function(filepath) {
+
+dat = read.csv(filepath, header = TRUE)
 
 dat1 = cc_cap(dat,"decimalLongitude","decimalLatitude") ## Identify Coordinates in Vicinity of Country Capitals
-
 dat2 = cc_cen(dat1,"decimalLongitude","decimalLatitude") ## Identify Coordinates in Vicinity of Country and Province Centroids
-
 dat3 = cc_equ(dat2,"decimalLongitude","decimalLatitude") ## Identify Records with Identical lat/lon
-
 dat4 = cc_gbif(dat3,"decimalLongitude","decimalLatitude") ## Identify Records Assigned to GBIF Headquarters
-
 dat5 = cc_inst(dat4,"decimalLongitude","decimalLatitude") ## Identify Records in the Vicinity of Biodiversity Institutions
+#??? = cc_sea(dat5,"decimalLongitude","decimalLatitude") ## Identify Non-terrestrial Coordinates
+dat6 = cc_val(dat5,"decimalLongitude","decimalLatitude") ## Identify Invalid lat/lon Coordinates
+dat7 = cc_zero(dat6,"decimalLongitude","decimalLatitude") ## Identify Zero Coordinates
+#??? = cc_outl(dat8, lon = "decimalLongitude", lat = "decimalLatitude",species="acceptedScientificName", method = "distance", tdi = 1000) ## Identify Geographic Outliers in Species Distributions
 
-dat6 = cc_sea(dat5,"decimalLongitude","decimalLatitude") ## Identify Non-terrestrial Coordinates
+file_name = paste0(strsplit(strsplit(filepath,"/")[[1]][length(strsplit(filepath,"/")[[1]])],".csv"),"_clean.csv")
+file_location = paste(strsplit(filepath,"/")[[1]][1:(length(strsplit(filepath,"/")[[1]])-1)],collapse='/')
+output_location = paste(c(file_location,file_name),collapse="/")
 
-dat7 = cc_val(dat6,"decimalLongitude","decimalLatitude") ## Identify Invalid lat/lon Coordinates
+removed_records = nrow(dat) - nrow(dat7)
+message("")
+message(paste("Removed",removed_records,'of',nrow(dat),'records.'))
+message()
 
-dat8 = cc_zero(dat7,"decimalLongitude","decimalLatitude") ## Identify Zero Coordinates
+write.csv(dat7,output_location)
 
-# dat9 = cc_outl(dat8, lon = "decimalLongitude", lat = "decimalLatitude",species="acceptedScientificName", method = "distance", tdi = 1000) ## Identify Geographic Outliers in Species Distributions
+}
