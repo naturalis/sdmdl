5cdc3d910c4dcb147f507d526ff6a9a57485ab9b
Winand <winand@live.nl>
2019-08-01 16:52:23 +0200

    Improvements to OccurrenceCleaner.R and testing with data_prep.py
    Includes various tif raster files that have been created by the data_prep script.

--- script/r/OccurrenceCleaner.R
+++ script/r/OccurrenceCleaner.R
@@ -1,28 +1,130 @@
 library("CoordinateCleaner")
 
-OccurenceCleaner <- function(filepath) {
+create_taxalist = function(dataPath)
+  
+{
+  
+  lidir = list.dirs(dataPath,recursive = TRUE)
+  lidir = lidir[2:length(lidir)]
+  
+  taxalist = c()
+  pathlist = c()
+  
+  for (l in lidir) {
+    for (i in list.files(l)) {
+      if (grepl(".csv",i)) { taxalist = append(taxalist,strsplit(i,".csv")[[1]]); pathlist = append(pathlist, l) }
+    }
+  }
+  
+  return(list(taxalist,pathlist))
+  
+}
+
+num.decimals <- function(x) {
+  stopifnot(class(x)=="numeric")
+  x <- sub("0+$","",x)
+  x <- sub("^.+[.]","",x)
+  nchar(x)
+}
+
+check.precision = function(lat,lon) 
+  
+{
+  lat_prcsn = num.decimals(lat)
+  lon_prcsn = num.decimals(lon)
+  
+  ifelse(lat_prcsn > 1 & lon_prcsn > 1, return(TRUE), return(FALSE))
+}
+
+check.boundingbox = function(lat,lon)
+  
+{
+  
+  ifelse(lat < -89.0 | lat > 89.0, return(FALSE), ifelse(lon < -179.0 | lon > 179.0,return(FALSE),return(TRUE)))
+  
+}
+
+OccurenceCleaner = function(dataPath,outPath) {
+
+tl = create_taxalist(dataPath)
+
+dir.create(file.path(outPath), showWarnings = FALSE)
+
+for (each in 1:length(tl[[1]]))
+  
+{
+
+filepath = paste0(tl[[2]][each],"/",tl[[1]][each],".csv")
 
 dat = read.csv(filepath, header = TRUE)
 
-dat1 = cc_cap(dat,"decimalLongitude","decimalLatitude") ## Identify Coordinates in Vicinity of Country Capitals
-dat2 = cc_cen(dat1,"decimalLongitude","decimalLatitude") ## Identify Coordinates in Vicinity of Country and Province Centroids
-dat3 = cc_equ(dat2,"decimalLongitude","decimalLatitude") ## Identify Records with Identical lat/lon
-dat4 = cc_gbif(dat3,"decimalLongitude","decimalLatitude") ## Identify Records Assigned to GBIF Headquarters
-dat5 = cc_inst(dat4,"decimalLongitude","decimalLatitude") ## Identify Records in the Vicinity of Biodiversity Institutions
-#??? = cc_sea(dat5,"decimalLongitude","decimalLatitude") ## Identify Non-terrestrial Coordinates
-dat6 = cc_val(dat5,"decimalLongitude","decimalLatitude") ## Identify Invalid lat/lon Coordinates
-dat7 = cc_zero(dat6,"decimalLongitude","decimalLatitude") ## Identify Zero Coordinates
-#??? = cc_outl(dat8, lon = "decimalLongitude", lat = "decimalLatitude",species="acceptedScientificName", method = "distance", tdi = 1000) ## Identify Geographic Outliers in Species Distributions
+message(paste("Cleaning",nrow(dat),sub("_"," ",tl[[1]][each],),"occurrences"))
 
-file_name = paste0(strsplit(strsplit(filepath,"/")[[1]][length(strsplit(filepath,"/")[[1]])],".csv"),"_clean.csv")
-file_location = paste(strsplit(filepath,"/")[[1]][1:(length(strsplit(filepath,"/")[[1]])-1)],collapse='/')
-output_location = paste(c(file_location,file_name),collapse="/")
+message("Testing coordinate precision")
 
-removed_records = nrow(dat) - nrow(dat7)
+dat$latlon = TRUE
+
+dat1 = dat
+
+for (row in 1:nrow(dat1))
+  
+{
+  
+  dat1$latlon[row] = check.precision(dat1$decimalLatitude[row],dat1$decimalLongitude[row])
+  
+}
+
+dat1 = dat1[dat1[,"latlon"] == TRUE,]
+
+dat1["latlon"] = NULL
+
+message(paste("Removed",nrow(dat)-nrow(dat1),"records."))
+
+dat1$boundbox = TRUE
+
+message("Testing bounding-box edge cases")
+
+dat2 = dat1
+
+for (row in 1:nrow(dat2))
+  
+{
+
+    dat2$boundbox[row] = check.boundingbox(dat2$decimalLatitude[row],dat2$decimalLongitude[row])
+  
+}
+
+dat2 = dat2[dat2[,"boundbox"] == TRUE,]
+
+dat2["boundbox"] = NULL
+
+message(paste("Removed",nrow(dat1)-nrow(dat2),"records."))
+
+message("Testing for overlapping occurrences")
+
+dat3 = dat2[row.names(unique(dat2[,c("decimalLatitude", "decimalLongitude")])),]
+
+message(paste("Removed",nrow(dat2)-nrow(dat3),"records."))
+
+dat4 = cc_cap(dat3,"decimalLongitude","decimalLatitude") ## Identify Coordinates in Vicinity of Country Capitals
+dat5 = cc_cen(dat4,"decimalLongitude","decimalLatitude") ## Identify Coordinates in Vicinity of Country and Province Centroids
+dat6 = cc_equ(dat5,"decimalLongitude","decimalLatitude") ## Identify Records with Identical lat/lon
+dat7 = cc_gbif(dat6,"decimalLongitude","decimalLatitude") ## Identify Records Assigned to GBIF Headquarters
+dat8 = cc_inst(dat7,"decimalLongitude","decimalLatitude") ## Identify Records in the Vicinity of Biodiversity Institutions
+dat9 = cc_sea(dat8,"decimalLongitude","decimalLatitude") ## Identify Non-terrestrial Coordinates
+dat10 = cc_val(dat9,"decimalLongitude","decimalLatitude") ## Identify Invalid lat/lon Coordinates
+dat11 = cc_zero(dat10,"decimalLongitude","decimalLatitude") ## Identify Zero Coordinates
+dat12 = cc_outl(dat11, lon = "decimalLongitude", lat = "decimalLatitude",species="acceptedScientificName", method = "distance", tdi = 1000) ## Identify Geographic Outliers in Species Distributions
+
+removed_records = nrow(dat) - nrow(dat12)
 message("")
 message(paste("Removed",removed_records,'of',nrow(dat),'records.'))
 message()
 
-write.csv(dat7,output_location)
+write.csv(dat12,paste0(outPath,"/",tl[[1]][each],".csv"))
+
+}
+
+write(tl[[1]],paste0(outPath,"/taxalist.txt"))
 
 }
