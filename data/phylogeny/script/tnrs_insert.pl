c1973bc6064e70c4764f3e6e1ef092f47bf3034a
Rutger Vos <rutgeraldo@gmail.com>
2019-08-12 15:16:15 +0200

    does tnrs on myco tables, inserts data

---
+++ data/phylogeny/script/tnrs_insert.pl
@@ -0,0 +1,109 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use DBI;
+use My::Taxon;
+use Getopt::Long;
+use Data::Dumper;
+use Bio::DB::Taxonomy;
+use Bio::Phylo::Util::Logger ':simple';
+
+# process command line arguments
+my $dbfile;
+my $infile;
+my $column;
+my $sep = '_';
+my $verbosity = WARN;
+GetOptions(
+	'sep=s'    => \$sep,
+	'dbfile=s' => \$dbfile,
+	'infile=s' => \$infile,
+	'column=s' => \$column,
+	'verbose+' => \$verbosity,
+);
+
+# make connections
+Bio::Phylo::Util::Logger->VERBOSE( '-level' => $verbosity );
+my $db = My::Taxon->connect( $dbfile )->resultset( 'Taxon' );
+my $entrez = Bio::DB::Taxonomy->new( '-source' => 'entrez' );
+
+# iterate over lines
+open my $fh, '<', $infile or die $!;
+my $attempts = 1;
+my @header;
+NAME: while(<$fh>) {
+	
+	# preprocess records into hashes
+	chomp;
+	my @line = split /\t/, $_;
+	if ( not @header ) {
+		@header = @line;
+		next NAME;
+	}
+	my %record = map { $header[$_] => $line[$_] } 0 .. $#header;
+	
+	# the input that has the taxon name
+	my $input = $record{$column};
+	
+	# need to go easy on Entrez API: sleep 10 seconds every 400 queries
+	if ( ( $attempts % 400 ) == 0 ) {
+		WARN "NEED TO SLEEP A BIT";
+		sleep(10);
+	}
+	
+	# check if literally in local DB
+	my $sanitized = $input;
+	$sanitized =~ s/\Q$sep\A/_/g; # local DB has underscores	
+	next NAME if db_check($sanitized);	
+	
+	# fetch canonical from NCBI
+	$input =~ s/\Q$sep\A/ /g; # NCBI has spaces
+	INFO "Going to query Entrez for $input";
+	my $taxonid;
+	eval { $taxonid = $entrez->get_taxonid($input) };
+	if ( $@ ) {
+		ERROR "Couldn't Entrez query $input: $@";
+		next NAME;
+	}	
+	if ( $taxonid ) {
+		$attempts++;
+		$input =~ s/ /$sep/g;
+		
+		# check if not 'No hit'
+		if ( $taxonid eq 'No hit' ) {
+			WARN "Entrez for $input: $taxonid";	
+			next NAME;
+		}
+	
+		# look up canonical name
+		my $taxon;
+		eval { $taxon = $entrez->get_taxon( '-taxonid' => $taxonid ) };
+		if ( $@ ) {
+			ERROR "Couldn't Entrez fetch $taxonid: $@";
+			next NAME;
+		}
+		my $canonical = $taxon->scientific_name;
+		$canonical =~ s/ /_/g;
+		WARN "No match for Entrez TNRS $input => $canonical" unless db_check($canonical);		
+	}
+}
+
+sub db_check {
+	my $name = shift;
+	if ( my $allmb = $db->search({ 'allmb_name' => $name })->single ) {
+		INFO "Found literal match $name";
+		# insert into database here
+		return 1;
+	}
+
+	# check if fuzzily in local DB
+	my $matches = 0;
+	my $allmb = $db->search({ 'allmb_name' => { '-like' => $name.'%' } });
+	while ( my $match = $allmb->next ) {
+		my $subsp = $match->allmb_name;
+		INFO "Found fuzzy match $name => $subsp";
+		# insert into database here
+		$matches++;
+	}
+	return $matches;
+}
