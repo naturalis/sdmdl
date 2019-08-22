59b749205f3abe3e1b3d474ff36efaf13fea0a91
Winand <winand@live.nl>
2019-08-22 17:04:59 +0200

    Update to handler classes.
    Changes to data_prep_handler and gis_handler.

--- sdmdl/sdmdl/data_prep_handler.py
+++ sdmdl/sdmdl/data_prep_handler.py
@@ -22,9 +22,12 @@ class data_prep_handler():
         
         self.create_presence_maps()        
         self.gis_handler.reload_tifs()        
-        #self.create_raster_stack()
+        self.create_raster_stack()
         self.raster_stack_clip()
         self.create_presence_pseudo_absence()
+        self.calc_band_mean_and_stddev()
+        self.create_training_df()
+        self.create_prediction_df()
         
     def create_presence_maps(self):
         
@@ -56,7 +59,7 @@ class data_prep_handler():
             os.makedirs(self.gis_handler.stack,exist_ok=True)         
         es.stack(self.gis_handler.variables, self.gis_handler.stack + '/stacked_env_variables.tif')                
         if self.verbose:
-            print(' Done!')
+            print(' Done!',end='\n')
             
     def raster_stack_clip(self):
     
@@ -172,3 +175,143 @@ class data_prep_handler():
             data.to_csv(self.gis_handler.spec_ppa + '/%s_ppa_dataframe.csv'%spec)
             
             
+    def calc_band_mean_and_stddev(self):
+                
+        raster=rasterio.open(self.gis_handler.stack + '/stacked_env_variables.tif')
+        profile=raster.profile
+        
+        with open(self.gis_handler.gis + '/env_bio_mean_std.txt','w+') as file:
+            file.write("band"+"\t"+"mean"+"\t"+"std_dev"+"\n")
+            file.close()
+            
+        for i in (tqdm.tqdm(range(1,self.gis_handler.length),desc='Computing band means and standard deviations' + (6 * ' ')) if self.verbose else range(1,self.gis_handler.length)):
+            profile.update(count=1)
+            band=raster.read(i)
+            band[band < -9999] = -9999
+            where_are_NaNs = np.isnan(band)
+            band[where_are_NaNs] = -9999
+            band_masked = np.ma.masked_array(band, mask=(band == -9999))
+        
+            #calculate mean and std.dev of each band
+            mean=band_masked.mean()
+            std_dev=np.std(band_masked)
+        
+            #write to file
+            with open(self.gis_handler.gis + '/env_bio_mean_std.txt','a') as file:
+                file.write(str(i)+"\t"+str(mean)+"\t"+str(std_dev)+"\n")
+                
+    def create_training_df(self):
+        
+        src=rasterio.open(self.gis_handler.stack + '/stacked_env_variables.tif')
+        inRas=gdal.Open(self.gis_handler.stack + '/stacked_env_variables.tif')        
+        for i in (tqdm.tqdm(self.occurrence_handler.name,desc='Creating training dataframe' + (23 * ' ')) if self.verbose else self.occurrence_handler.name):            
+            data = pd.read_csv(self.gis_handler.spec_ppa + '/%s_ppa_dataframe.csv'%i)
+            spec = data["taxon_name"][0]
+            spec = spec.replace(" ","_")
+            len_pd=np.arange(len(data))
+            long=data["dLon"]
+            lati=data["dLat"]
+            ppa=data["present/pseudo_absent"]        
+            lon=long.values
+            lat=lati.values        
+            row=[]
+            col=[]        
+            for i in len_pd:
+                row_n, col_n = src.index(lon[i], lat[i])# spatial --> image coordinates
+                row.append(row_n)
+                col.append(col_n)        
+            myarray=inRas.ReadAsArray()
+            mean_std=pd.read_csv(self.gis_handler.gis + '/env_bio_mean_std.txt',sep="\t")
+            mean_std=mean_std.to_numpy()
+            X=[]
+            species =["%s"%spec]*int(len(row))        
+            for j in range(0,self.occurrence_handler.length):
+                band=myarray[j]
+                x=[]
+                for i in range(0,len(row)):
+                    value= band[row[i],col[i]]
+                    if j < self.gis_handler.scaled_len:
+                        if value <-1000:
+                            value=np.nan
+                        else:  
+                            value = ((value - mean_std.item((j,1))) / mean_std.item((j,2)))#scale values
+                        x.append(value)                        
+                    if j >= self.gis_handler.scaled_len:
+                        if value <-1000:
+                            value=np.nan
+                        else:  
+                            value=value
+                        x.append(value)
+                X.append(x)
+            X =np.array([np.array(xi) for xi in X])
+            df=pd.DataFrame(X) 
+            df=df.T
+            df["present/pseudo_absent"]=ppa
+            df["dLat"]=lati
+            df["dLon"]=long
+            df["taxon_name"]=species
+            df["present/pseudo_absent"]=ppa
+            df["row_n"]=row
+            df.rename(columns=dict(zip(df.columns[0:self.occurrence_handler.length], self.occurrence_handler.name)),inplace=True)
+            df=df.dropna(axis=0, how='any')
+            input_data=df
+            if not os.path.isdir(self.gis_handler.spec_ppa_env):
+                os.makedirs(self.gis_handler.spec_ppa_env,exist_ok=True)
+            input_data.to_csv(self.gis_handler.spec_ppa_env + '/%s_env_dataframe.csv'%spec)
+    
+    def create_prediction_df(self):
+    
+        if self.verbose:
+            print('Creating prediction dataframe' + (21 * ' ') + ':',end='')
+        inRas=gdal.Open(self.gis_handler.stack + '/stacked_env_variables.tif')
+        myarray=inRas.ReadAsArray()
+        df=pd.read_csv(self.gis_handler.gis + '/world_locations_to_predict.csv')        
+        len_pd=np.arange(len(df))
+        lon=df["dLon"]
+        lat=df["dLat"]
+        lon=lon.values
+        lat=lat.values        
+        row=[]
+        col=[]        
+        src=rasterio.open(self.gis_handler.stack + '/stacked_env_variables.tif')        
+        for i in len_pd:
+            row_n, col_n = src.index(lon[i], lat[i])
+            row.append(row_n)
+            col.append(col_n)
+        mean_std=pd.read_csv(self.gis_handler.gis + '/env_bio_mean_std.txt',sep="\t")
+        mean_std=mean_std.to_numpy()
+        X=[]
+        for j in range(0,self.occurrence_handler.length):
+            band=myarray[j]
+            x=[]
+        
+            for i in range(0,len(row)):
+                if j < self.gis_handler.scaled_len:
+                    value= band[row[i],col[i]]
+                    value = ((value - mean_std.item((j,1))) / mean_std.item((j,2)))#scale values
+                    x.append(value)
+                if j >= self.gis_handler.scaled_len:
+                    value= band[row[i],col[i]]
+                    x.append(value)
+            X.append(x)
+        X.append(row)
+        X.append(col)
+        X =np.array([np.array(xi) for xi in X])
+        df=pd.DataFrame(X)
+        df=df.T
+        df.rename(columns=dict(zip(df.columns[0:self.occurrence_handler.length], self.occurrence_handler.name)),inplace=True)
+        df=df.dropna(axis=0, how='any')
+        df.head()
+        input_X=df.iloc[:,0:self.occurrence_handler.length]
+        np.shape(input_X)
+        row=df[self.occurrence_handler.length]
+        col=df[self.occurrence_handler.length+1]
+        row_col=pd.DataFrame({"row":row,"col":col})
+        input_X=input_X.values
+        row=row.values
+        col=col.values
+        np.save(self.gis_handler.gis + '/world_prediction_array.npy',input_X)
+        row_col.to_csv(self.gis_handler.gis + '/world_prediction_row_col.csv')
+        if self.verbose:
+            print(' Done!')
+                    
