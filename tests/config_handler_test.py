d09c209fc8f215257a8641492bee9f0e4b8516df
Winand <winand@live.nl>
2019-08-28 12:22:04 +0200

    Unittesting for config, gis and occurrence classes (uploaded in previous commit).

--- tests/config_handler_test.py
+++ tests/config_handler_test.py
@@ -1,41 +1,58 @@
 from sdmdl.sdmdl.config_handler import config_handler
+from sdmdl.sdmdl.occurrence_handler import occurrence_handler
+from sdmdl.sdmdl.gis_handler import gis_handler
+import yaml
 import unittest
 import os
-
 
 class ConfigHandlerTestSuite(unittest.TestCase):
     """Test cases for Config Handler class."""
 
     def setUp(self):
-        self.root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data'))
-        self.config = config_handler(self.root)
 
-    def test_instance_variables(self):
-        self.assertEqual(self.config.root, self.root)
-        self.assertEqual(self.config.config, self.root + '/config.yml')
-        self.assertEqual(self.config.data_path, self.root + '/data')
-        self.assertEqual(self.config.occ_path, self.root + '/data/occurrences')
-        self.assertEqual(self.config.result_path, self.root + '/results')
+        self.root = os.path.abspath(os.path.join(os.path.dirname(__file__))) + '/test_data'
+        self.oh = occurrence_handler(self.root + '/occurrence_handler')
+        self.gh = gis_handler(self.root + '/gis_handler')
+
+    def test__init__(self):
+        self.ch = config_handler(self.root + '/config_handler', self.oh, self.gh)
+        self.assertEqual(self.ch.oh,self.oh)
+        self.assertEqual(self.ch.gh,self.gh)
+        self.assertEqual(self.ch.root, self.root + '/config_handler')
 
     def test_search_config(self):
-        self.config.root = self.root + '/test'
+        self.ch = config_handler(self.root + '/config_handler', self.oh, self.gh)
+        self.ch.search_config()
+        self.assertEqual(self.ch.config,self.root + '/config_handler/config.yml')
         with self.assertRaises(IOError):
-            self.config.search_config()
-        self.config.root = self.root
-        self.config.search_config()
-        self.assertEqual(self.config.config, self.root + '/config.yml')
+            self.ch = config_handler(self.root + '/no_config', self.oh, self.gh)
+            self.ch.search_config()
+
+    def test_create_yaml(self):
+        self.ch = config_handler(self.root + '/config_handler', self.oh, self.gh)
+        self.ch.search_config()
+        self.ch.create_yaml()
+        with open(self.ch.config, 'r') as stream:
+            yml = yaml.safe_load(stream)
+        self.assertIsInstance(yml,dict)
+        self.assertEqual(list(yml.keys()),['data_path','occurrence_path','result_path','occurrences','layers'])
+        self.assertEqual(yml[list(yml.keys())[0]],self.root + '/config_handler/data')
+        self.assertEqual(yml[list(yml.keys())[1]],self.root + '/config_handler/data/occurrences')
+        self.assertEqual(yml[list(yml.keys())[2]],self.root + '/config_handler/results')
+        self.assertEqual(yml[list(yml.keys())[3]],dict(zip(self.oh.name, self.oh.path)))
+        self.assertEqual(yml[list(yml.keys())[4]],dict(zip(self.gh.names, self.gh.variables)))
 
     def test_read_yaml(self):
-        self.config.config = self.root + '/test/config.yml'
-        print(self.config.config)
-        with self.assertRaises(IOError):
-            self.config.read_yaml()
-        self.config.config = self.root + '/config.yml'
-        self.config.read_yaml()
-        self.assertEqual(self.config.data_path, self.root + '/data')
-        self.assertEqual(self.config.occ_path, self.root + '/data/occurrences')
-        self.assertEqual(self.config.result_path, self.root + '/results')
-
+        self.ch = config_handler(self.root + '/config_handler', self.oh, self.gh)
+        self.ch.search_config()
+        self.ch.read_yaml()
+        self.assertEqual(self.ch.data_path,self.root + '/config_handler/data')
+        self.assertEqual(self.ch.occ_path,self.root + '/config_handler/data/occurrences')
+        self.assertEqual(self.ch.result_path,self.root + '/config_handler/results')
+        self.assertEqual(self.ch.oh.name,list(dict(zip(self.oh.name, self.oh.path)).keys()))
+        self.assertEqual(self.ch.oh.path,list(dict(zip(self.oh.name, self.oh.path)).values()))
+        self.assertEqual(self.ch.gh.names,list(dict(zip(self.gh.names, self.gh.variables)).keys()))
+        self.assertEqual(self.ch.gh.variables,list(dict(zip(self.gh.names, self.gh.variables)).values()))
 
 if __name__ == '__main__':
     unittest.main()
