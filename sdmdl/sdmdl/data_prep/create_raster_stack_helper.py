cdb2929e5dc050203ff5091ef4b7685698a9b523
Rutger Vos <rutgeraldo@gmail.com>
2019-09-02 23:52:38 +0200

    Update create_raster_stack_helper.py

--- sdmdl/sdmdl/data_prep/create_raster_stack_helper.py
+++ sdmdl/sdmdl/data_prep/create_raster_stack_helper.py
@@ -1,9 +1,12 @@
 import earthpy.spatial as es
 import os
 
-
+# Why is this not called RasterStack?
+# What does this class do?
 class CreateRasterStackHelper():
 
+    # Why does this again have the same constructor? 
+    # This should use inheritance.
     def __init__(self, oh, gh, ch, verbose):
         self.oh = oh
         self.gh = gh
@@ -11,6 +14,8 @@ class CreateRasterStackHelper():
         self.verbose = verbose
 
     def create_raster_stack(self):
+        
+        # Why are we not using logging?
         if self.verbose:
             print('Creating raster stack' + (29 * ' ') + ':', end='')
         if not os.path.isdir(self.gh.stack):
